debf34bba425686827722aeb900c0b49c1a0d1dd
Alencar Rodrigo Hentges <alencar.hentges-ext@olxbr.com>
2022-09-23 16:25:39 -0300

    adding spring demo project with spring old version

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class DemoController {
+
+  @GetMapping
+  public String HelloWorld() {
+    return "Hello";
+  }
+}
